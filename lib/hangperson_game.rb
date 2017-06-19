de80fa1e7093f5e1823e2b0334a555510c7e58a9
matt182 <mattspring0@gmail.com>
2017-06-19 13:28:55 +0000

    heroku ready

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,52 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word, :guesses, :wrong_guesses, :word_with_guesses
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @word_with_guesses = ''
+    @try = 7
+    word.each_char {|letter| @word_with_guesses += '-'}
+  end
+  
+  def guess(letter)
+    if letter.nil? or not letter.match('[a-zA-Z]+') then raise ArgumentError end
+    letter.downcase!
+    if word.include? letter then
+      if not @guesses.include? letter then 
+        @guesses += letter
+        update_word_with_guesses(letter)
+      else 
+        return false 
+      end
+    else
+      if not @wrong_guesses.include? letter then 
+        @wrong_guesses += letter
+        @try -= 1
+      else
+        return false
+      end
+    end
+    true
+  end
+  
+  def update_word_with_guesses(letter)
+    @word.each_char.with_index do |val, index|
+      if val == letter then @word_with_guesses[index] = letter end
+    end 
+  end
+  
+  def check_win_or_lose
+    if @try == 0 then
+      return :lose
+    elsif word_with_guesses.include? '-' then
+      return :play
+    else
+      return :win
+    end
   end
 
   def self.get_random_word
